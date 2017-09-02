243e5de786bc6b38b33fc1b8f9182cfc2fc9548a
lpestl <lpestlname@gmail.com>
2017-09-02 04:52:09 +0300

    Simple zoom

---
+++ ZoomToCursor/ZoomForImageForm.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace ZoomToCursor
+{
+    partial class ZoomForImageForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureEditZoom = new DevExpress.XtraEditors.PictureEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEditZoom.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureEditZoom
+            // 
+            this.pictureEditZoom.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureEditZoom.Cursor = System.Windows.Forms.Cursors.Default;
+            this.pictureEditZoom.EditValue = global::ZoomToCursor.Properties.Resources.ido;
+            this.pictureEditZoom.Location = new System.Drawing.Point(12, 12);
+            this.pictureEditZoom.Name = "pictureEditZoom";
+            this.pictureEditZoom.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.pictureEditZoom.Properties.ShowCameraMenuItem = DevExpress.XtraEditors.Controls.CameraMenuItemVisibility.Auto;
+            this.pictureEditZoom.Properties.MouseWheel += new System.Windows.Forms.MouseEventHandler(this.pictureEdit1_Properties_MouseWheel_1);
+            this.pictureEditZoom.Size = new System.Drawing.Size(608, 435);
+            this.pictureEditZoom.TabIndex = 0;
+            this.pictureEditZoom.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureEditZoom_Paint);
+            // 
+            // ZoomForImageForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(632, 459);
+            this.Controls.Add(this.pictureEditZoom);
+            this.Name = "ZoomForImageForm";
+            this.Text = "Сделать zoom to cursor для XtraEditors.PictureEdit(Devexpress)";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureEditZoom.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PictureEdit pictureEditZoom;
+    }
+}
+
---
+++ ZoomToCursor/ZoomForImageForm.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace ZoomToCursor
+{
+    public partial class ZoomForImageForm : DevExpress.XtraEditors.XtraForm
+    {
+        // Оригинал картинки
+        private Image imgOrigin;
+        // Graphics созданный из pictureEditZoom
+        private Graphics graph;
+        // Хранит текущий размер картинки
+        private double currentSizeW;
+        private double currentSizeH;
+        // Хранит координаты картинки
+        private double currentPointX;
+        private double currentPointY;
+        // Zoom линейный. Зависит от размера изображения и от этого коэффициента зума
+        public double kZoom = 10.0f;
+        // Cоотношения сторон картинки-оригинала
+        private double ratio;
+
+        public ZoomForImageForm()
+        {
+            InitializeComponent();
+
+            // Инициализация вспомогательных переменных
+            imgOrigin = pictureEditZoom.Image;
+
+            currentSizeW = pictureEditZoom.Image.Size.Width;
+            currentSizeH = pictureEditZoom.Image.Size.Height;
+
+            ratio = currentSizeW / currentSizeH;
+
+            currentPointX = (currentSizeW - pictureEditZoom.Width) / 2 * (-1);
+            currentPointY = (currentSizeH - pictureEditZoom.Height) / 2 * (-1);
+        }
+
+        // Добавить к pictureEdit событие MouseWheel
+        private void pictureEdit1_Properties_MouseWheel_1(object sender, MouseEventArgs e)
+        {
+            Console.WriteLine("Mouse pos (x:{0}, y:{1}) Wheel delta:{2}", e.X, e.Y, e.Delta);
+
+            double lastSizeW = currentSizeW;
+            double lastSizeH = currentSizeH;
+            
+            // Здесь можно всяко разно поизвращаться со способами зума
+            currentSizeW += currentSizeW / e.Delta * kZoom;
+            currentSizeH += currentSizeH / e.Delta * kZoom;
+
+            // Для сохранения соотношения сторон при маленьком размере
+            if (currentSizeW < 1)
+            {
+                currentSizeW = 1;
+                currentSizeH = currentSizeW / ratio;
+            }
+            if (currentSizeH < 1)
+            {
+                currentSizeH = 1;
+                currentSizeW = currentSizeH * ratio;
+            }
+
+            // Разница в размерах
+            double deltaW = lastSizeW - currentSizeW;
+            double deltaH = lastSizeH - currentSizeH;
+            // Считаем новый сдвиг картинки, относительно курсора мышки
+            currentPointX += deltaW * (e.X - currentPointX) / lastSizeW;
+            currentPointY += deltaH * (e.Y - currentPointY) / lastSizeH;
+            // Обновляем картинку
+            pictureEditZoom.Refresh();
+        }
+
+        // Добавляем pictureEdit обработчик собития Paint
+        private void pictureEditZoom_Paint(object sender, PaintEventArgs e)
+        {
+            // Получаем графику в pictureEdit
+            graph = e.Graphics;
+            // Очищаем
+            graph.Clear(pictureEditZoom.BackColor);
+            // Создаем новую картинку из оригинала с новыми размерами
+            Bitmap bmp = new Bitmap(imgOrigin, new Size((int)currentSizeW, (int)currentSizeH));
+            // Рисуем по сдвигу, относительно курсора
+            graph.DrawImage(bmp, new Point((int)currentPointX, (int)currentPointY));
+        }
+    }
+}
